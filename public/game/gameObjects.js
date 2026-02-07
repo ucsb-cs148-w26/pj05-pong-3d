6810b364d09c4ac6bf0ba8b6086c495d9cb87463
s-cavalier <jrc974@ucsb.edu>
2026-02-06 19:53:02 -0800

    bc-ballisnowsphereeee

--- public/game/gameObjects.js
+++ public/game/gameObjects.js
@@ -64,7 +64,7 @@ export class Arena {
 
 export class Ball {
 	constructor(paddle1, paddle2, scores) {
-		const geometry = new THREE.BoxGeometry(1, 1, 1);
+		const geometry = new THREE.SphereGeometry(0.5);
 		const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
 		this.visual = new THREE.Mesh(geometry, material);
 		this.visual.castShadow = true;
