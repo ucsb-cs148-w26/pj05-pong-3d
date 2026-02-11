e80c6cf352a8d38ad18cf81ea728f18f4f7730c2
Wong Zhao <193191274+wongzhao@users.noreply.github.com>
2026-02-11 13:40:42 -0800

    WZ - remove a bunch of magic numbers (copilot)

--- public/game/gameObjects.js
+++ public/game/gameObjects.js
@@ -4,6 +4,28 @@ import { Vec3 } from '../physics/math.js';
 import { RigidBody } from '../physics/engine.js';
 import { KeyboardController } from './controllers.js';
 import { BodyForceApplier } from '../physics/forces.js';
+import {
+	PADDLE_THICKNESS,
+	PADDLE_HEIGHT,
+	PADDLE_DEPTH,
+	PADDLE_ACCEL,
+	PADDLE_BOUND,
+	PADDLE_BOUND_ADJUST,
+	PADDLE_VELOCITY_DAMPING,
+	ARENA_DEPTH,
+	ARENA_SIZE,
+	WALL_THICKNESS,
+	STATIC_MASS,
+	ARENA_END_OFFSET,
+	ARENA_WALL_OFFSET_Y,
+	ARENA_WALL_OFFSET_Z,
+	ARENA_COLOR,
+	BALL_RADIUS,
+	BALL_COLOR,
+	BALL_MASS,
+	BALL_INITIAL_SPEED,
+	BALL_TINY_V_SCALE
+} from './constants.js';
 
 export class Paddle {
 	// Square paddle
@@ -12,34 +34,37 @@ export class Paddle {
 		bodyIdentifier,
 		controller = new KeyboardController('yz')
 	) {
-		const geometry = new THREE.EdgesGeometry(new THREE.BoxGeometry(0.5, 3, 3));
+		const geometry = new THREE.EdgesGeometry(
+			new THREE.BoxGeometry(PADDLE_THICKNESS, PADDLE_HEIGHT, PADDLE_DEPTH)
+		);
 		const material = new THREE.LineBasicMaterial(meshSettings);
 
 		this.visual = new THREE.LineSegments(geometry, material);
 		this.visual.castShadow = true;
 		this.visual.receiveShadow = true;
-		this.body = new RigidBody(99999);
+		this.body = new RigidBody(STATIC_MASS);
 		this.body.ballIdentifier = bodyIdentifier;
-		this.body.col = new BoxCollider(0.5, 3, 3, this.body.transform);
+		this.body.col = new BoxCollider(
+			PADDLE_THICKNESS,
+			PADDLE_HEIGHT,
+			PADDLE_DEPTH,
+			this.body.transform
+		);
 		this.controller = controller;
-		this.accel = 40;
+		this.accel = PADDLE_ACCEL;
 		this.forceApplier = new BodyForceApplier(this.body, (vec) => {});
 	}
 
-	get position() {
-		return this.body.x;
-	}
-
 	update(dt) {
-		if (this.body.x.y > 5.75) this.body.x.y = 5.65;
-		if (this.body.x.y < -5.75) this.body.x.y = -5.65;
-		if (this.body.x.z > 5.75) this.body.x.z = 5.65;
-		if (this.body.x.z < -5.75) this.body.x.z = -5.65;
+		if (this.body.x.y > PADDLE_BOUND) this.body.x.y = PADDLE_BOUND_ADJUST;
+		if (this.body.x.y < -PADDLE_BOUND) this.body.x.y = -PADDLE_BOUND_ADJUST;
+		if (this.body.x.z > PADDLE_BOUND) this.body.x.z = PADDLE_BOUND_ADJUST;
+		if (this.body.x.z < -PADDLE_BOUND) this.body.x.z = -PADDLE_BOUND_ADJUST;
 
 		let direction = this.controller.checkMoveInputs();
 		if (direction === null) direction = new Vec3();
 
-		direction.addVec(this.body.v.clone().scale(-0.25));
+		direction.addVec(this.body.v.clone().scale(PADDLE_VELOCITY_DAMPING));
 
 		direction.scale(this.accel * this.body.m);
 		this.forceApplier.applier = (f) => {
@@ -48,14 +73,12 @@ export class Paddle {
 	}
 }
 
-const PI = 3.14159265359;
-
 export class Arena {
 	//Long Hallway Arena
 	constructor(physicsEngine, scores) {
-		const geometry = new THREE.BoxGeometry(23.5, 15, 15); // x, y, z
+		const geometry = new THREE.BoxGeometry(ARENA_DEPTH, ARENA_SIZE, ARENA_SIZE); // x, y, z
 		const material = new THREE.MeshStandardMaterial({
-			color: 0x222222,
+			color: ARENA_COLOR,
 			side: THREE.BackSide
 		});
 		this.visual = new THREE.Mesh(geometry, material);
@@ -63,31 +86,61 @@ export class Arena {
 		this.visual.receiveShadow = true;
 
 		this.bodies = [
-			new RigidBody(999999),
-			new RigidBody(999999),
-			new RigidBody(999999),
-			new RigidBody(999999),
-			new RigidBody(999999),
-			new RigidBody(999999)
+			new RigidBody(STATIC_MASS),
+			new RigidBody(STATIC_MASS),
+			new RigidBody(STATIC_MASS),
+			new RigidBody(STATIC_MASS),
+			new RigidBody(STATIC_MASS),
+			new RigidBody(STATIC_MASS)
 		];
 
 		// MeshCollider should probably be created so this is more convenient
 		// but i aint doin allat rn
 
-		this.bodies[0].col = new BoxCollider(23.5, 3, 15, this.bodies[0].transform);
-		this.bodies[1].col = new BoxCollider(23.5, 3, 15, this.bodies[1].transform);
-		this.bodies[2].col = new BoxCollider(23.5, 15, 3, this.bodies[2].transform);
-		this.bodies[3].col = new BoxCollider(23.5, 15, 3, this.bodies[3].transform);
+		this.bodies[0].col = new BoxCollider(
+			ARENA_DEPTH,
+			WALL_THICKNESS,
+			ARENA_SIZE,
+			this.bodies[0].transform
+		);
+		this.bodies[1].col = new BoxCollider(
+			ARENA_DEPTH,
+			WALL_THICKNESS,
+			ARENA_SIZE,
+			this.bodies[1].transform
+		);
+		this.bodies[2].col = new BoxCollider(
+			ARENA_DEPTH,
+			ARENA_SIZE,
+			WALL_THICKNESS,
+			this.bodies[2].transform
+		);
+		this.bodies[3].col = new BoxCollider(
+			ARENA_DEPTH,
+			ARENA_SIZE,
+			WALL_THICKNESS,
+			this.bodies[3].transform
+		);
 
-		this.bodies[4].col = new BoxCollider(3, 15, 15, this.bodies[4].transform);
-		this.bodies[5].col = new BoxCollider(3, 15, 15, this.bodies[5].transform);
+		this.bodies[4].col = new BoxCollider(
+			WALL_THICKNESS,
+			ARENA_SIZE,
+			ARENA_SIZE,
+			this.bodies[4].transform
+		);
+		this.bodies[5].col = new BoxCollider(
+			WALL_THICKNESS,
+			ARENA_SIZE,
+			ARENA_SIZE,
+			this.bodies[5].transform
+		);
 
-		this.bodies[0].x.addVec(new Vec3(0, 9));
-		this.bodies[1].x.addVec(new Vec3(0, -9));
-		this.bodies[2].x.addVec(new Vec3(0, 0, 9));
-		this.bodies[3].x.addVec(new Vec3(0, 0, -9));
-		this.bodies[4].x.addVec(new Vec3(-13.28125));
-		this.bodies[5].x.addVec(new Vec3(13.28125));
+		this.bodies[0].x.addVec(new Vec3(0, ARENA_WALL_OFFSET_Y));
+		this.bodies[1].x.addVec(new Vec3(0, -ARENA_WALL_OFFSET_Y));
+		this.bodies[2].x.addVec(new Vec3(0, 0, ARENA_WALL_OFFSET_Z));
+		this.bodies[3].x.addVec(new Vec3(0, 0, -ARENA_WALL_OFFSET_Z));
+		this.bodies[4].x.addVec(new Vec3(-ARENA_END_OFFSET));
+		this.bodies[5].x.addVec(new Vec3(ARENA_END_OFFSET));
 
 		this.bodies[4].ballIdentifier = 'greenWall';
 		this.bodies[5].ballIdentifier = 'redWall';
@@ -103,22 +156,25 @@ export class Arena {
 
 export class Ball {
 	constructor(scores) {
-		const geometry = new THREE.SphereGeometry(0.5);
-		const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+		const geometry = new THREE.SphereGeometry(BALL_RADIUS);
+		const material = new THREE.MeshStandardMaterial({ color: BALL_COLOR });
 		this.visual = new THREE.Mesh(geometry, material);
 		this.visual.castShadow = true;
 		this.scores = scores;
 		this.needsToReset = false;
-		this.body = new RigidBody(3);
+		this.body = new RigidBody(BALL_MASS);
 		this.body.col = new SphereCollider(
-			0.5,
+			BALL_RADIUS,
 			this.body.transform,
 			(me, other) => {
 				if (!Object.hasOwn(other, 'ballIdentifier')) return;
 
 				switch (other.ballIdentifier) {
 					case 'paddle': {
-						const tinyV = this.body.v.clone().normalize().scale(0.1);
+						const tinyV = this.body.v
+							.clone()
+							.normalize()
+							.scale(BALL_TINY_V_SCALE);
 						this.body.v.addVec(tinyV);
 						return;
 					}
@@ -136,19 +192,19 @@ export class Ball {
 			}
 		);
 
-		this.body.v.assign(5, 0, 0);
+		this.body.v.assign(BALL_INITIAL_SPEED, 0, 0);
 	}
 
 	reset() {
 		this.body.x.assign(0, 0, 0);
 
-		let theta = (Math.random() * PI) / 2 + PI / 4;
+		let theta = (Math.random() * Math.PI) / 2 + Math.PI / 4;
 
 		const thetaDir = 2 * Math.floor(Math.random() * 2) - 1;
 
 		theta *= thetaDir;
 
-		let phi = (Math.random() * PI) / 2 + PI / 4;
+		let phi = (Math.random() * Math.PI) / 2 + Math.PI / 4;
 
 		this.body.v
 			.assign(
@@ -156,7 +212,7 @@ export class Ball {
 				Math.cos(phi),
 				Math.cos(theta) * Math.sin(phi)
 			)
-			.scale(5);
+			.scale(BALL_INITIAL_SPEED);
 	}
 
 	update(dt) {
